74a50416f95ca32397816d55f176e66a558b333c
deepivin <marek12306@gmail.com>
2021-10-11 19:30:54 +0200

    base url

--- json_gen.rb
+++ json_gen.rb
@@ -5,6 +5,7 @@ dir = Dir["./images/*"]
 
 json = {
     count: dir.length,
+    baseURL: "https://raw.githubusercontent.com/marek12306/lao-gan-ma/main/images/",
     files: dir.map { |f| File.basename(f) }
 }.to_json
 
